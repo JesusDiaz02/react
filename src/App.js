2964e65306731c35adae04cbd858c98f4d9d5f7f
jesus diaz <jesusdiazg9@gmail.com>
2022-04-19 16:32:38 -0500

    second

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Variables from './components/Variables';
-import Contador from './components/Contador';
-import Listas from './components/Listas';
-import Eventos from './components/Eventos';
 import Formulario from './components/Formulario';
 
 function App() {
   return (
-    <div>
-      
-        
-         <Variables/>
+    <div>        
          <Formulario/>
-      
     </div>
   );
 }
